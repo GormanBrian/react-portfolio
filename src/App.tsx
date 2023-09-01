9b6bd6490627d74eb7a235e23564812d7e5b320b
Brian Gorman <briangorman99@gmail.com>
2023-09-01 16:16:11 -0400

    setup react-router-dom, add pages folder, add AboutMe, Contact, Portfolio, and Resume pages, remove index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
 import Header from "./components/Header";
 
 function App() {
+  const navigate = useNavigate();
+  const activePath = useLocation().pathname;
+
+  useEffect(() => {
+    if (activePath === "/") {
+      navigate("/AboutMe");
+    }
+  }, [activePath, navigate]);
+
   return (
     <>
       <Header />
-      <></>
+      <Outlet />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import App from "./App.tsx";
+import { AboutMe, Portfolio, Contact, Resume } from "./pages";
 import theme from "./theme.ts";
 
-import App from "./App.tsx";
-import "./index.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/about-me",
+        element: <AboutMe />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+      {
+        path: "/Portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/Resume",
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
